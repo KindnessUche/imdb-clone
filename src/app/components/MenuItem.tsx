cfc62b90ea5c93dd13463ec8d967a5f40e38633e
KindnessUche <davidkindness10@gmail.com>
2024-12-13 19:28:23 +0100

    Add About page

--- src/app/components/MenuItem.tsx
+++ src/app/components/MenuItem.tsx
@@ -13,7 +13,7 @@ export default function MenuItem({
   Icon: IconType;
 }) {
   return (
-    <Link href={address} className="hover:text-amber-500 text-orange-600">
+    <Link href={address} className="hover:text-amber-500">
       <Icon className="text-2xl sm:hidden" />
       <p className="uppercase hidden sm:inline text-sm">{title}</p>
     </Link>
