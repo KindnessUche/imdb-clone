8a1b8dc11c70bf661464572095cf7d07da62886d
KindnessUche <davidkindness10@gmail.com>
2024-12-19 06:55:17 +0100

    error fixed

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { FiThumbsUp } from "react-icons/fi";
 
-export default function Card({ result }: any) {
+export default function Card({ result }) {
   return (
     <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 ">
       <Link href={`/movie/${result.id}`}>
